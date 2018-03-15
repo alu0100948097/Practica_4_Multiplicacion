98ff29266ddabb9da78bd66a76a9837cf4f39051
alu0100948097 <alu0100948097@ull.edu.es>
2018-03-15 21:25:47 +0000

    Implementado desplazamiento

--- lib/multiplicacion.rb
+++ lib/multiplicacion.rb
@@ -20,4 +20,13 @@ class Multiplicacion
         @algoritmo=a.hex.to_s(2).rjust(8,'0')
     end
     
+    def izq(b)
+        temp=b.split("")
+        for i in (0..temp.length-2)
+            temp[i]=temp[i+1]
+        end
+        temp[temp.length-1]="0"
+        return temp.join
+    end
+end
 end
